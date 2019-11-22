14ee7a97fa55e45580bbdf953dc67d16b77ecf4f
maridabul <mariana.dabul@poli.ufrj.br>
2019-11-22 08:44:36 -0200

    alteracoes finais

--- intermediario.pl
+++ intermediario.pl
@@ -7,5 +7,30 @@ use lib dirname (__FILE__) . "/modulo";
 
 use funcoesPerl;
 
+sub criarArquivoPalavrasNaoEncontradas {
+  
+  my $arquivoInput = $_[0];
+  chomp $arquivoInput;
+  my $indiceNaoEncontradas = 0;
+  my @arrayInput = importarArquivoParaArray ($arquivoInput);
+  my @arrayNaoEncontradas = verificarArrayPalavrasNoDicionario (@arrayInput); 
+  
+  my $arquivoPalavrasNaoEncontradas = "arquivoPalavrasNaoEncontradas.txt";
+
+  open(ESCRITA, ">", $arquivoPalavrasNaoEncontradas) or die $!;
+  
+  # escrever no arquivo as palavras nao encontradas no dicionario
+  foreach (@arrayNaoEncontradas)
+  {
+    #$_ = $arrayNaoEncontradas[$indiceNaoEncontradas];
+    #print ESCRITA $_;
+    print ESCRITA ($arrayNaoEncontradas[$indiceNaoEncontradas]);
+    print ESCRITA "\n";
+    $indiceNaoEncontradas += 1;
+  }
+
+  close(ESCRITA);
+  return $arquivoPalavrasNaoEncontradas;
+}
   
 1;
